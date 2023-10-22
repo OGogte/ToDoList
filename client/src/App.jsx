a82f72ea9a96f34fcde125f762498031f7bc62df
OGogte <omg.omgogte@gmail.com>
2023-10-22 21:09:23 +0530

    Added Frontend functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,130 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+
+const API_BASE = "http://localhost:3001";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    GetTodos();
+  }, []);
+
+  const GetTodos = async () => {
+    try {
+      const response = await fetch(`${API_BASE}/todos`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch todos");
+      }
+      const data = await response.json();
+      setTodos(data);
+    } catch (err) {
+      console.error("Error: ", err);
+    }
+  }
+
+  const completeTodo = async (id) => {
+    try {
+      const response = await fetch(`${API_BASE}/todo/complete/${id}`);
+      if (!response.ok) {
+        throw new Error("Failed to complete todo");
+      }
+      const data = await response.json();
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) =>
+          todo._id === data._id ? { ...todo, complete: data.complete } : todo
+        )
+      );
+    } catch (err) {
+      console.error("Error: ", err);
+    }
+  }
+
+  const deleteTodo = async (id) => {
+    try {
+      const response = await fetch(`${API_BASE}/todo/delete/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Failed to delete todo");
+      }
+      const data = await response.json();
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo._id !== data._id));
+    } catch (err) {
+      console.error("Error: ", err);
+    }
+  }
+
+  const addTodo = async () => {
+    try {
+      const response = await fetch(`${API_BASE}/todo/new`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text: newTodo,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to add todo");
+      }
+      const data = await response.json();
+      setTodos((prevTodos) => [...prevTodos, data]);
+      setPopupActive(false);
+      setNewTodo("");
+    } catch (err) {
+      console.error("Error: ", err);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h1>Welcome, Om</h1>
+      <h4>Your Tasks</h4>
+
+      <div className="todos">
+        {todos.map((todo) => (
+          <div
+            className={"todo " + (todo.complete ? "is-complete" : "")}
+            key={todo._id}
+            onClick={() => completeTodo(todo._id)}
+          >
+            <div className="checkbox"></div>
+            <div className="text">{todo.text}</div>
+            <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>
+              x
+            </div>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="addPopup" onClick={() => setPopupActive(true)}>
+        +
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      {popupActive && (
+        <div className="popup">
+          <div className="closePopup" onClick={() => setPopupActive(false)}>
+            x
+          </div>
+          <div className="content">
+            <h3>Add Task</h3>
+            <input
+              type="text"
+              className="add-todo-input"
+              onChange={(e) => setNewTodo(e.target.value)}
+              value={newTodo}
+            />
+            <div className="button" onClick={addTodo}>
+              Create Task
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
